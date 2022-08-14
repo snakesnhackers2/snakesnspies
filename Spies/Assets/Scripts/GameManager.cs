508d9d7924aaf8a847fe28459895f0590f03073b
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2022-08-14 17:16:41 +0800

    Update game manager a bit

--- Spies/Assets/Scripts/GameManager.cs
+++ Spies/Assets/Scripts/GameManager.cs
@@ -372,49 +372,53 @@ public class GameManager : MonoBehaviour
             if (!mainEndTurn && !combatCardSelected)
             {
                 // process the card being used
-                CardUsed(cardSelected.cardName);
+                string cardValidation = CardUsed(cardSelected.cardName);
 
-                // add card back into main deck
-                deck.Add(cardSelected);
-
-                if (gamestate == "mainmap")
+                if (cardValidation == "ValidCard")
                 {
-                    // remove the card from inventory
-                    players[mainPlayerTurn - 1].availableCardSlots[cardSelected.posInHand] = false;
 
-                    players[mainPlayerTurn - 1].inventoryDeck.Remove(cardSelected);
+                    // add card back into main deck
+                    deck.Add(cardSelected);
 
-                    // reorganize inventory (ie move remaining cards to center with for loop
-                    int i = 0;
-                    foreach (Card inventoryCard in players[mainPlayerTurn - 1].inventoryDeck)
+                    if (gamestate == "mainmap")
                     {
-                        // move into place and scale
-                        inventoryCard.transform.position = playerInventories[0].cardSlots[i].position;
-                        inventoryCard.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                        i++;
+                        // remove the card from inventory
+                        players[mainPlayerTurn - 1].availableCardSlots[cardSelected.posInHand] = false;
+
+                        players[mainPlayerTurn - 1].inventoryDeck.Remove(cardSelected);
+
+                        // reorganize inventory (ie move remaining cards to center with for loop
+                        int i = 0;
+                        foreach (Card inventoryCard in players[mainPlayerTurn - 1].inventoryDeck)
+                        {
+                            // move into place and scale
+                            inventoryCard.transform.position = playerInventories[0].cardSlots[i].position;
+                            inventoryCard.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                            i++;
+                        }
                     }
-                }
-                else
-                {
-                    TotalDamageLeft.text = "Total Attack:\n" + damageDealt[0].ToString();
-                    TotalDamageRight.text = "Total Attack:\n" + damageDealt[1].ToString();
-
-                    // remove the card from inventory
-                    players[playerOrder[combatPlayerTurn] - 1].availableCardSlots[cardSelected.posInHand] = false;
-
-                    players[playerOrder[combatPlayerTurn] - 1].inventoryDeck.Remove(cardSelected);
-
-                    // reorganize inventory (ie move remaining cards to center with for loop
-                    int i = 0;
-                    foreach (Card inventoryCard in players[playerOrder[combatPlayerTurn] - 1].inventoryDeck)
+                    else
                     {
-                        // move into place and scale
-                        inventoryCard.transform.position = playerInventories[0].cardSlots[i].position;
-                        inventoryCard.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-                        inventoryCard.posInHand = i;
-                        i++;
+                        TotalDamageLeft.text = "Total Attack:\n" + damageDealt[0].ToString();
+                        TotalDamageRight.text = "Total Attack:\n" + damageDealt[1].ToString();
+
+                        // remove the card from inventory
+                        players[playerOrder[combatPlayerTurn] - 1].availableCardSlots[cardSelected.posInHand] = false;
+
+                        players[playerOrder[combatPlayerTurn] - 1].inventoryDeck.Remove(cardSelected);
+
+                        // reorganize inventory (ie move remaining cards to center with for loop
+                        int i = 0;
+                        foreach (Card inventoryCard in players[playerOrder[combatPlayerTurn] - 1].inventoryDeck)
+                        {
+                            // move into place and scale
+                            inventoryCard.transform.position = playerInventories[0].cardSlots[i].position;
+                            inventoryCard.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                            inventoryCard.posInHand = i;
+                            i++;
+                        }
+                        players[playerOrder[combatPlayerTurn] - 1].inventoryDeck.Add(cardSelected);
                     }
-                    players[playerOrder[combatPlayerTurn] - 1].inventoryDeck.Add(cardSelected);
                 }
 
             }
@@ -501,7 +505,7 @@ public class GameManager : MonoBehaviour
     }
 
     // TODO
-    public void CardUsed(string cardName)
+    public string CardUsed(string cardName)
     {
         bool invalidCard = true;
 
@@ -572,6 +576,7 @@ public class GameManager : MonoBehaviour
                 default:
                     break;
             }
+
         } else
         {
             if (!combatCardSelected)
@@ -649,11 +654,13 @@ public class GameManager : MonoBehaviour
             Debug.Log("This card cannot be used here!");
 
             // play some sound effect to indicate it is invalid & 
+            return "InvalidCard";
         } else
         {
             // TODO (KIV)
             // play some sound effect to indicate it is been selected successfully
             // or add like sparkles or something in unity
+            return "ValidCard";
         }
     }
 
