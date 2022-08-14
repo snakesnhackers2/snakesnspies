d2ce9c98e6b6dd7f7681e79474e4ea6f2612294b
franceneee <57038559+franceneee@users.noreply.github.com>
2022-08-14 10:44:16 +0800

    Add more card attributes

--- Spies/Assets/Scripts/Card.cs
+++ Spies/Assets/Scripts/Card.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public string cardName;
-
-
+    public int damage;
+    public float defense; //percentage
+    public int hpBoost;
+    public int rounds; //number of rounds before getting to play again
 }
