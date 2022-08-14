9b48498e0f1551d1bce2f5fd020e8359485b9746
franceneee <57038559+franceneee@users.noreply.github.com>
2022-08-14 11:17:55 +0800

    Add movement logic

--- Spies/Assets/Scripts/PlayerMove.cs
+++ Spies/Assets/Scripts/PlayerMove.cs
@@ -14,18 +14,33 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            System.Random random = new System.Random();
+            int steps = random.Next(1, 6);
+            Move(steps);
+        }
     }
 
-    public void Move(int steps) {
-        if (currentTile + steps > 100) {
+    public void Move(int steps)
+    {
+        Debug.Log($"{this.transform.name} move {steps}");
+        if (currentTile + steps > 100)
+        {
+            currentTile = 100;
             Transform player = this.transform;
             GameObject destination = GameObject.Find($"Tower/100");
-            player.parent = destination.gameObject.transform;
-        }/*  else if (currentTile - steps < 1) {
+            player.SetParent(destination.gameObject.transform);
+        }/*  else if (currentTile - steps < 1) { // when they're on the way back down
             Transform player = this.transform;
             GameObject destination = GameObject.Find($"Tower/100");
             player.parent = destination.gameObject.transform;
         } */
-
+        else {
+            currentTile += steps;
+            Transform player = this.transform;
+            GameObject destination = GameObject.Find($"Tower/{currentTile}");
+            player.SetParent(destination.gameObject.transform);
+        }
     }
 }
